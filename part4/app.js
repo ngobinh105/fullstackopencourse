c5086efd55972e563827f776c88dbe531a9fc52c
ngobinh105 <binhngo1005@gmail.com>
2021-09-21 11:35:00 +0300

    submit exercise 4.1-4.2

---
+++ part4/app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const mongoose = require('mongoose')
+const middleware = require('./utils/middleware')
+const blogRouter = require('./controllers/blogs')
+
+logger.info('connecting to', config.MONGODB_URI)
+mongoose
+  .connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs', blogRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
