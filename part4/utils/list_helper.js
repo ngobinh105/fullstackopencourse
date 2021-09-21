5beeeea6f0543f40cb3897dfd152d2fcde1cd26a
ngobinh105 <binhngo1005@gmail.com>
2021-09-21 13:51:45 +0300

    submit exercise 4.3-4.7

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,4 +1,56 @@
 const dummy = (blogs) => {
   return 1
 }
-module.exports = { dummy }
+
+const totalLikes = (list) => {
+  return list.reduce((acc, cur) => {
+    return acc + cur.likes
+  }, 0)
+}
+
+const favoriteBlog = (bloglists) => {
+  const favBlog = bloglists.reduce((acc, cur) => {
+    if (acc.likes > cur.likes) {
+      return acc
+    } else {
+      return cur
+    }
+  })
+  return {
+    title: favBlog.title,
+    author: favBlog.author,
+    likes: favBlog.likes,
+  }
+}
+const mostBlogs = (bloglists) => {
+  const uniqueAuthors = [...new Set(bloglists.map((item) => item.author))].map(
+    (author) => ({
+      author,
+      blogs: bloglists.filter((blog) => blog.author === author).length,
+    })
+  )
+  return uniqueAuthors.reduce((acc, cur) => {
+    if (acc.blogs > cur.blogs) {
+      return acc
+    } else return cur
+  })
+}
+const mostLikes = (bloglists) => {
+  const uniqueAuthors = [...new Set(bloglists.map((item) => item.author))].map(
+    (author) => ({
+      author,
+      likes: bloglists.reduce((acc, cur) => {
+        if (cur.author === author) {
+          return acc + cur.likes
+        } else return acc
+      }, 0),
+    })
+  )
+  return uniqueAuthors.reduce((acc, cur) => {
+    if (acc.likes > cur.likes) {
+      return acc
+    } else return cur
+  })
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog, mostBlogs, mostLikes }
